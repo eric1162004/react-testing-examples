e53f118b9924af99147355ba0cc46d8c6e7b3cbe
Eric Cheung <eric1162004@hotmail.com>
2024-06-08 18:18:57 -0700

    simplfying test setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "jsdom",
+    globals: true, // so you dont need to import vitest 
+    setupFiles: "tests/setup.ts",
   },
 });
