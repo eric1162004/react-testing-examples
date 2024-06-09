1c01f75a9f5db0a127835d66d6f72023eddb103a
Eric Cheung <eric1162004@hotmail.com>
2024-06-08 22:46:42 -0700

    simplying code

--- tests/components/OrderStatusSelector.test.tsx
+++ tests/components/OrderStatusSelector.test.tsx
@@ -9,31 +9,33 @@ import userEvent from "@testing-library/user-event";
 // npm i -D resize-observer-polyfill
 
 describe("OrderStatusSelector", () => {
-  it("should render New as the default value", () => {
+  const renderComponent = () => {
     render(
       <Theme>
         <OrderStatusSelector onChange={vi.fn()} />
       </Theme>
     );
 
-    // const button = screen.getByRole('button') // In the Radix component, the button has custom role of combobox
-    const button = screen.getByRole("combobox");
-    expect(button).toHaveTextContent(/new/i);
+    return {
+      trigger: screen.getByRole("combobox"),
+      getOtions: () => screen.findAllByRole("option"),
+    };
+  };
+
+  it("should render New as the default value", () => {
+    const { trigger } = renderComponent();
+
+    expect(trigger).toHaveTextContent(/new/i);
   });
 
   it("should render corrent statuses", async () => {
-    render(
-      <Theme>
-        <OrderStatusSelector onChange={vi.fn()} />
-      </Theme>
-    );
+    const { trigger, getOtions } = renderComponent();
 
-    const button = screen.getByRole("combobox");
     const user = userEvent.setup();
-    await user.click(button);
+    await user.click(trigger);
 
     // the list option appear asynchronously, so use findBy
-    const options = await screen.findAllByRole("option");
+    const options = await getOtions();
     expect(options).toHaveLength(3);
     const labels = options.map((option) => option.textContent); // HTMLElement has property textContent
     expect(labels).toEqual(["New", "Processed", "Fulfilled"]);
