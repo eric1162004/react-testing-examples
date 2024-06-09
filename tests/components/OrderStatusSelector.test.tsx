70c76725c3001e91b7d8775df46d2578a3e0f85f
Eric Cheung <eric1162004@hotmail.com>
2024-06-08 23:06:55 -0700

    testing OrderStatusSelector

--- tests/components/OrderStatusSelector.test.tsx
+++ tests/components/OrderStatusSelector.test.tsx
@@ -10,15 +10,21 @@ import userEvent from "@testing-library/user-event";
 
 describe("OrderStatusSelector", () => {
   const renderComponent = () => {
+    const onChange = vi.fn();
+
     render(
       <Theme>
-        <OrderStatusSelector onChange={vi.fn()} />
+        <OrderStatusSelector onChange={onChange} />
       </Theme>
     );
 
     return {
       trigger: screen.getByRole("combobox"),
-      getOtions: () => screen.findAllByRole("option"),
+      getOptions: () => screen.findAllByRole("option"),
+      getOption: (label: RegExp) =>
+        screen.findByRole("option", { name: label }),
+      user: userEvent.setup(),
+      onChange,
     };
   };
 
@@ -29,9 +35,8 @@ describe("OrderStatusSelector", () => {
   });
 
   it("should render corrent statuses", async () => {
-    const { trigger, getOtions } = renderComponent();
+    const { trigger, getOptions: getOtions, user } = renderComponent();
 
-    const user = userEvent.setup();
     await user.click(trigger);
 
     // the list option appear asynchronously, so use findBy
@@ -39,5 +44,35 @@ describe("OrderStatusSelector", () => {
     expect(options).toHaveLength(3);
     const labels = options.map((option) => option.textContent); // HTMLElement has property textContent
     expect(labels).toEqual(["New", "Processed", "Fulfilled"]);
+  });
+
+  it.each([
+    { label: /processed/i, value: "processed" },
+    { label: /fulfilled/i, value: "fulfilled" },
+  ])(
+    "should call onChange with $value when the $label option is selected",
+    async ({ label, value }) => {
+      const { trigger, user, onChange, getOption } = renderComponent();
+      await user.click(trigger);
+
+      const option = await getOption(label);
+      await user.click(option);
+
+      expect(onChange).toHaveBeenCalledWith(value);
+    }
+  );
+
+  it("should call onChange with `new` when the `New` option is selected", async () => {
+    const { trigger, user, onChange, getOption } = renderComponent();
+
+    await user.click(trigger);
+    const processedOption = await getOption(/processed/i);
+    await user.click(processedOption);
+
+    await user.click(trigger);
+    const newOption = await getOption(/new/i);
+    await user.click(newOption);
+
+    expect(onChange).toHaveBeenCalledWith("new");
   });
 });
