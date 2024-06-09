1b95e4b64a78509a60dbbab5e1d6afd74ce5c106
Eric Cheung <eric1162004@hotmail.com>
2024-06-08 22:27:25 -0700

    working with component libraries

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,4 +1,12 @@
 import '@testing-library/jest-dom/vitest'
+import ResizeObserver from 'resize-observer-polyfill'
+
+// require for testing Radix UI lib
+global.ResizeObserver = ResizeObserver;
+
+window.HTMLElement.prototype.scrollIntoView = vi.fn();
+window.HTMLElement.prototype.hasPointerCapture = vi.fn();
+window.HTMLElement.prototype.releasePointerCapture = vi.fn();
 
 // add this property to the window object in the testing env
 Object.defineProperty(window, 'matchMedia', {
