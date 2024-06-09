7eef7e226f7a5a39d8f1c8d03f1bfbb902357800
Eric Cheung <eric1162004@hotmail.com>
2024-06-08 21:49:52 -0700

    Testing asynchronous code

---
+++ tests/components/TagList.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import TagList from '../../src/components/TagList';
+
+describe('TagList', () => {
+  it('should render tags', async () => {
+
+        render(<TagList/>)
+
+        // // waitFor keeps calling the callback until timeout
+        // await waitFor(()=> { 
+        //     // codes here should not cause side effects
+        //     const listItems = screen.getAllByRole('listitem')
+        //     expect(listItems.length).toBeGreaterThan(0)
+        // })
+
+        // findAllByRole = waitFor + getAllByRole
+        const listItems = await screen.findAllByRole('listitem')
+        expect(listItems.length).toBeGreaterThan(0)
+  });
+});
